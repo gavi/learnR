a441f33133705f5f7d98cc212ddd932fe6635bfc
Gavi Narra <gavi.narra@objectgraph.com>
2014-04-21 16:17:08 -0400

    some new files added

---
+++ stockchart.R
@@ -0,0 +1,14 @@
+stockdata <- function(ticker){
+	require(ggplot2);
+	url <- paste("http://ichart.finance.yahoo.com/table.csv?s=",ticker,sep="");
+	stock_data <- read.csv(url);
+	stock_data$Date <- as.Date(stock_data$Date);
+	return(stock_data);
+}
+
+stockchart <- function(ticker){
+	stock_data <- stockdata(ticker);
+	output<-qplot(Date,Close,data=stock_data);
+	print(output);
+	
+}
