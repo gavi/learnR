67e792472b8a33fe2203af84bc271210f01cd72b
Gavi Narra <gavi.narra@objectgraph.com>
2014-04-22 17:48:28 -0400

    comments added

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,11 +1,22 @@
+## Calculates the pollutant mean by reading the CSV files in 'specdata' folder
+## Pass the spec data and any vector with in the 1:332 range
+## Does not check for valid input
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  #How do you read all the files in a directory
+  #You can read all the files in a directory by using list.files as below
   #files<-list.files(directory,pattern="*.csv",full.names=TRUE)
   pdata<-NULL
-  files<-sprintf("%s/%.3d.csv",directory,id)
+  
+  #I chose to construct it using sprintf
+  files<-sprintf("%s/%03d.csv",directory,id)
   for(file in files){
     pdata<-rbind(pdata,read.csv(file))
   }
   round(mean(pdata[[pollutant]],na.rm=TRUE),digits=3)
 }
 
+# Example output
+# > pollutantmean("specdata","sulfate",1:10)
+# [1] 4.064
+# > pollutantmean("specdata","nitrate",1:10)
+# [1] 0.798
